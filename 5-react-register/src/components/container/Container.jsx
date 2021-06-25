8b420c5096f0e225cce4146544e4331d0f4e93dd
medium0fry <medium0fry@gmail.com>
2021-06-25 17:56:08 -0400

    Created Container component to help organize app

---
+++ 5-react-register/src/components/container/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './container.css';
+
+const Container = ({component, size = 'sm', children}) => {
+   return (React.createElement(component, {
+      className: `container ${size}`
+   }, children))
+}
+
+export default Container
