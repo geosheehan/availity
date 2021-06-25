c36cb898ecf0349575252cbfa3f81ca27063a565
medium0fry <medium0fry@gmail.com>
2021-06-25 19:48:12 -0400

    Moved Field to a fragment.

--- 5-react-register/src/components/field/Field.jsx
+++ 5-react-register/src/components/field/Field.jsx
@@ -1,18 +1,17 @@
 import './field.css'
 
-const Field = ({label, type, id, rows, style}) => {
+const Field = ({label, type, id, rows}) => {
    id = id ?? label;
    type = type || "text";
    return (
-      <div className="field" style={style}>
+      <>
          <label htmlFor={id}>{label}</label>
          {
             type === "textarea"
             ? <textarea rows={rows || 1}></textarea>
             : <input id={id} type={type}  />
-
          }
-      </div>
+      </>
    )
 }
 
