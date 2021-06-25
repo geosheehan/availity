65e9c5a16bbb2b5fd84f8d6f7970242d0156132e
medium0fry <medium0fry@gmail.com>
2021-06-25 19:44:41 -0400

    Form applies css classes to form elements

--- 5-react-register/src/components/form/Form.jsx
+++ 5-react-register/src/components/form/Form.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import './form.css';
 
 const Form = ({children}) => {
-   console.log({children});
    return (
       <form>
          {children.map(child => {
-            const width = Math.min(Math.max(child.props.col ?? 12, 1), 12) / 12 * 100;
-            return React.cloneElement(child, {...child.props, style: {width: `${width}%`}});
+            const col = `c${Math.min(Math.max(child.props.col ?? 12, 1), 12)}`;
+
+            return (
+               <div className={`field ${col}`}>
+                  {child}
+               </div>
+            )
          })}
       </form>
    )
