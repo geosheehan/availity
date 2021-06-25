dc5a7064316741272661729d15d4741951728f4a
medium0fry <medium0fry@gmail.com>
2021-06-25 17:57:23 -0400

    Updated app to use new components

--- 5-react-register/src/App.jsx
+++ 5-react-register/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css';
 
+import Container from './components/container/Container';
+import Field from './components/field/Field';
+import Form from './components/form/Form';
+
 function App() {
   return (
-    <main>
-    </main>
+
+    <Container component="main">
+      <Form>
+        <Field label="First Name" col={6}/>
+        <Field label="Last Name" col={6}/>
+        <Field label="NPI Number" />
+        <Field label="Business Address" type="textarea" rows={4}/>
+        <Field label="Telephone Number" type="tel" />
+        <Field label="Email Address" type="email" />
+        <button type="submit">Sign Up</button>
+      </Form>
+    </Container>
   );
 }
 
