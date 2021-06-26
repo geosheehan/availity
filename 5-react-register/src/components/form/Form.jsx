21a058b60bb6523a0810af98be5395f8af7d6119
medium0fry <medium0fry@gmail.com>
2021-06-26 08:37:59 -0400

    Adding submit handling. Fixed react warning about keyless children

--- 5-react-register/src/components/form/Form.jsx
+++ 5-react-register/src/components/form/Form.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import './form.css';
 
-const Form = ({children}) => {
+const Form = ({children, onSubmit}) => {
    return (
-      <form>
-         {children.map(child => {
+      <form onSubmit={onSubmit}>
+         {children.map((child, index) => {
             const col = `c${Math.min(Math.max(child.props.col ?? 12, 1), 12)}`;
 
             return (
-               <div className={`field ${col}`}>
+               <div key={index} className={`field ${col}`}>
                   {child}
                </div>
             )
