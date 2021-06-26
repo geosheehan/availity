31461352168fe424d55f25be83dd308dc2db7bf8
medium0fry <medium0fry@gmail.com>
2021-06-25 21:11:41 -0400

    Added section headers to the form

--- 5-react-register/src/App.jsx
+++ 5-react-register/src/App.jsx
@@ -9,13 +9,20 @@ function App() {
 
     <Container component="main">
       <Form>
+        <h3>Personal Information</h3>
         <Field label="First Name" col={6}/>
         <Field label="Last Name" col={6}/>
-        <Field label="NPI Number" />
-        <Field label="Business Address" type="textarea" rows={4}/>
         <Field label="Telephone Number" type="tel" />
         <Field label="Email Address" type="email" />
-        <button type="submit">Sign Up</button>
+        <Field label="NPI Number" />
+
+        <h3>Business Address</h3>
+        <Field label="Street Address" type="textarea" rows={2}/>
+        <Field label="City" col={12} />
+        <Field label="State" col={9}/>
+        <Field label="Zip Code" col={3}/>
+
+        <button type="submit" col={4}>Sign Up</button>
       </Form>
     </Container>
   );
